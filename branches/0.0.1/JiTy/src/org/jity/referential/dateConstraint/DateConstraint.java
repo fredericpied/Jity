e1fd68e0b775ca7c81e47cc483c9f8985465c5f9
frepie <frepie@ef3bb5c2-181f-44a6-a9f7-ce529ae7e690>
2012-06-13 06:32:54 +0000

    Share project "JiTy" into "http://subversion.assembla.com/svn/jity/branches/0.0.1"

--- branches/0.0.1/JiTy/src/org/jity/referential/dateConstraint/DateConstraint.java
+++ branches/0.0.1/JiTy/src/org/jity/referential/dateConstraint/DateConstraint.java
@@ -4,14 +4,23 @@ import java.util.Date;
 
 import org.jity.referential.ExecConstraint;
 
+/**
+ * Represents an planification constraint. 
+ * The constraint est validated when the planification rule is true and the minimal start time
+ * in current valid day is over
+ * @author 09344a
+ *
+ */
 public class DateConstraint extends ExecConstraint {
 	
+	/**
+	 * Planification rules
+	 */
 	private PlanifRule planifRule;
-
 	
 	/**
-	 * Minimal start time in the day
+	 * Minimal start time in the day ("HH:MM")
 	 */
-	private Date minStartTime;
+	private String minStartTime;
 	
 }
