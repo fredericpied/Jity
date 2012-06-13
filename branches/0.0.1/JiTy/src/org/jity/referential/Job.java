63f77cf5aded2e7dc73b5c58a3aca04d7439c35c
frepie <frepie@ef3bb5c2-181f-44a6-a9f7-ce529ae7e690>
2012-06-13 10:08:54 +0000

    Add standard class header

--- branches/0.0.1/JiTy/src/org/jity/referential/Job.java
+++ branches/0.0.1/JiTy/src/org/jity/referential/Job.java
@@ -1,3 +1,27 @@
+/**
+ *  JiTy : Open Job Scheduler
+ *  Copyright (C) 2012 
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free
+ *  Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ *  MA 02111-1307, USA
+ *
+ *  For questions, suggestions:
+ *
+ *  http://www.assembla.com/spaces/jity
+ *
+ */
 package org.jity.referential;
 
 import java.util.ArrayList;
@@ -16,7 +40,7 @@ public class Job {
 	private String description;
 
 	/**
-	 * Command to exécute on execMachine
+	 * Command to execute on execMachine
 	 */
 	private String command;
 	
@@ -24,10 +48,10 @@ public class Job {
 	 * Execution status of the job: "WAITING", "EXECUTING", "FAILED", "SUCCESS"
 	 */
 	private int status;
-	private static final int WAITING_STATUS=1;
-	private static final int EXECUTING_STATUS=2;
-	private static final int FAILED_STATUS=3;
-	private static final int SUCCESSED_STATUS=4;
+	private static final int WAITING_STATUS = 1;
+	private static final int EXECUTING_STATUS = 2;
+	private static final int FAILED_STATUS = 3;
+	private static final int SUCCESSED_STATUS = 4;
 	
 	/**
 	 * If false, the execution of this job is not permitted
