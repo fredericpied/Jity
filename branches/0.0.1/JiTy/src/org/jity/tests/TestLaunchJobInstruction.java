3cf09150693e18f41596a454185274fb446f0e00
frepie <frepie@ef3bb5c2-181f-44a6-a9f7-ce529ae7e690>
2012-07-23 20:21:26 +0000

    Adding SWT lib

--- branches/0.0.1/JiTy/src/org/jity/tests/TestLaunchJobInstruction.java
+++ branches/0.0.1/JiTy/src/org/jity/tests/TestLaunchJobInstruction.java
@@ -29,9 +29,6 @@ public class TestLaunchJobInstruction extends TestCase {
 			logger.info("Waiting 5 sec");
 			TimeUtil.waiting(5);
 			
-		} catch (AgentException e) {
-			e.printStackTrace();
-			System.exit(1);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 			System.exit(1);
